b7725c95dde5019fe9073c495e9e78111e69c243
Robert Smidt <rsmidt909@hotmail.com>
2019-05-31 11:23:14 -0500

    created super hero project, created hero class

---
+++ SuperHeroes/Models/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string MyProperty { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
